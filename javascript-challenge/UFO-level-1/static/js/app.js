d7a34f01ec57530f8dd3739d9af34cad9effb31b
Brandon <brandonsu1729@gmail.com>
2020-06-22 21:28:06 -0700

    HW 14 Javascript

---
+++ javascript-challenge/UFO-level-1/static/js/app.js
@@ -0,0 +1,69 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+var ufoTable = d3.select("tbody");;
+
+for (var i = 0; i < tableData.length; i++){
+	var row = ufoTable.append("tr");
+
+	var cell = row.append("td");
+	cell.text(tableData[i].datetime);
+
+	cell = row.append("td");
+	cell.text(tableData[i].city);
+
+	cell = row.append("td");
+	cell.text(tableData[i].state);
+
+	cell = row.append("td");
+	cell.text(tableData[i].country);
+
+	cell = row.append("td");
+	cell.text(tableData[i].shape);
+
+	cell = row.append("td");
+	cell.text(tableData[i].durationMinutes);
+
+	cell = row.append("td");
+	cell.text(tableData[i].comments);
+}
+
+var filterButton =  d3.select("#filter-btn");
+
+var inputField = d3.select("#datetime");
+var newText = "";
+
+inputField.on("change", function(){
+	newText = d3.event.target.value;
+});
+function onClick(){
+	console.log(newText);
+	for (var i = 0; i < tableData.length; i++){
+		if (tableData[i].datetime === newText){
+			var row = ufoTable.append("tr");
+
+			var cell = row.append("td");
+			cell.text(tableData[i].datetime);
+
+			cell = row.append("td");
+			cell.text(tableData[i].city);
+
+			cell = row.append("td");
+			cell.text(tableData[i].state);
+
+			cell = row.append("td");
+			cell.text(tableData[i].country);
+
+			cell = row.append("td");
+			cell.text(tableData[i].shape);
+
+			cell = row.append("td");
+			cell.text(tableData[i].durationMinutes);
+
+			cell = row.append("td");
+			cell.text(tableData[i].comments);
+		}
+	}
+}
+filterButton.on("click", onClick);

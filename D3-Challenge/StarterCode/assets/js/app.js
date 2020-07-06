eda29c9e61b4b12c4114d2d6fdb340c8dabc9dc7
Brandon <brandonsu1729@gmail.com>
2020-07-05 17:12:43 -0700

    D3 HW 16 Second Upload

--- D3-Challenge/StarterCode/assets/js/app.js
+++ D3-Challenge/StarterCode/assets/js/app.js
@@ -16,27 +16,44 @@ var height = svgHeight - margin.top - margin.bottom;
 var width = svgWidth - margin.left - margin.right;
 
 var svg = d3
-	.select("body")
+	.select("#scatter")
 	.append("svg")
 	.attr("height", svgHeight)
-	.attr("width", svgWidth);
-
-var chartGroup = svg.append("g")
-	.attr("transform", `translate(${margin.left}, ${margin.top})`);
-
-
-
+	.attr("width", svgWidth)
+	.append("g")
+	.attr("transform", `translate(${margin.left}, ${margin.top})`);;
 
 d3.csv("./assets/data/data.csv").then(function(data){
-	console.log(data);
 
 	data.forEach(function(data){
-		healthCare.push(data.healthcare);
+		healthCare.push(+data.healthcare);
 	});
 	data.forEach(function(data){
-		pov.push(data.poverty);
+		pov.push(+data.poverty);
 	});
 	
+	var x = d3.scaleLinear()
+		.domain([0, d3.max(pov) + 5])
+		.range([0, width]);
+	svg.append("g")
+		.attr("transform", "translate(0, " + height + ")")
+		.call(d3.axisBottom(x));
+
+	var y = d3.scaleLinear()
+		.domain([d3.max(healthCare) + 5, 0])
+		.range([height, 0]);
+	svg.append("g")
+		.call(d3.axisLeft(y));
+
+	svg.append('g')
+		.selectAll("dot")
+		.data(data)
+		.enter()
+		.append("circle")
+			.attr("cx", 5.0)
+			.attr("cy", 5.0)
+			.attr("r", 5)
+			.style("fill", "#69b3a2")
 });
 
 console.log(healthCare);
